e3b983b3f292553e9b0514227b51e9eee2c64a92
ShaikBahirAkram24 <shaikbahirakram@gmail.com>
2024-06-04 11:51:49 +0530

    Delete final directory

--- final/index.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll(".thumbnailContainer").forEach(function(container) {
-    container.addEventListener("click", function() {
-      var url = container.getAttribute("data-url");
-      window.location.href = url;
-    });
-  });
-
-  
